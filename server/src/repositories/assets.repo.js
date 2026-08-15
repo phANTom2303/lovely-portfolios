70f7c16986ca3b448a845dda3e6b00e71e603c9a
Fate-Death <pavitrarajpoot64@gmail.com>
2026-08-16 00:40:01 +0530

    created assets repo and services

--- server/src/repositories/assets.repo.js
+++ server/src/repositories/assets.repo.js
@@ -1,26 +1,21 @@
 import { query } from "#src/config/db.js";
-
 export const findAll = async () => {
-  const sql = `SELECT * FROM links`;
+  const sql = ' SELECT * FROM assets';
   const { rows } = await query(sql);
   return rows;
 };
 
-export const create = async (link, title, description = null) => {
-  const sql = `
-    INSERT INTO links (link, title, description)
-    VALUES ($1, $2, $3) 
-    RETURNING *
-    `;
-
-  const { rows } = await query(sql, [link, title, description]);
-
+export const create = async (title, link, description = null, asset_type) => {
+  const sql = `INSERT INTO assets (title, link, description)
+  VALUES ($1,$2,$3,$4) RETURNING *`;
+  const { rows } = await query(sql, [title, link, description, asset_type]);
   return rows[0];
 };
 
+
 export const remove = async (id) => {
   const sql = `
-    DELETE FROM links
+    DELETE FROM assets
     WHERE id=$1
     RETURNING *
     `;
@@ -30,27 +25,20 @@ export const remove = async (id) => {
 };
 
 export const update = async (id, fields) => {
-  const allowedKeys = ['title', 'link', 'description'];
+  const allowedKeys = ['title', 'link', 'description', 'asset_type'];
   let setClauses = [];
   let values = [];
   let paramIndex = 1;
-  for (const key of allowedKeys) {
+  for (const key in allowedKeys) {
     if (fields[key] !== undefined) {
       setClauses.push(`${key}=$${paramIndex}`);
       values.push(fields[key]);
       paramIndex++;
     }
   }
-
   values.push(id);
-
-  const sql = `
-    UPDATE links
-    SET ${setClauses.join(', ')}
-    WHERE id = $${paramIndex}
-    RETURNING *;
-    `;
-
+  const sql = `UPDATE assets SET ${setClauses.join(', ')}
+  where id = $${paramIndex} RETURNING *`;
   const { rows } = await query(sql, values);
   return rows[0] ?? null;
 };
